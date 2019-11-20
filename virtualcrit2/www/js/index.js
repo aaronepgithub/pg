28d69bd1362d3358f5f9c1f7ca9bcd991cf65b63
AE <aaronep@live.com>
2019-11-20 14:30:41 -0500

    Init

---
+++ virtualcrit2/www/js/index.js
@@ -0,0 +1,59 @@
+var app = new Framework7({
+    root: '#app',
+    name: 'app',
+    id: 'app',
+    
+    material: true,
+    cache: true,
+    init: true,
+    initOnDeviceReady: true,
+    on: {
+        init: function () {
+            console.log('App initialized');
+            startup();
+        },
+    }
+});
+
+var $$ = Dom7;
+
+var scannedDevices = [];
+function postScan() {
+    console.log('postScan');
+    console.log(JSON.stringify(scannedDevices));
+
+    scannedDevices.forEach(element => {
+        console.log(element.id + ", " + element.rssi);
+        $$('.p1').html(element.id);
+    });
+}
+
+function startup() {
+    console.log('startup function');
+    scannedDevices = [];
+    ble.scan([], 5, function(device) {
+        console.log(JSON.stringify(device));
+        scannedDevices.push(device);
+    }, function(e) {
+        console.log('failure, ' + e);
+    });
+
+    setTimeout(function() {
+        console.log('scan complete, calling postScan');
+        postScan();
+    },
+        5000
+    );
+
+}
+
+
+
+
+// Handle Cordova Device Ready Event
+// $$(document).on('deviceready', function () {
+//     console.log('Device is ready!');
+//     startup();
+// });
+
+
